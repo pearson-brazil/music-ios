8c91b41aa02674b8e85eeae3d5f805e28b14723d
Pearson <w.policiano@gmail.com>
2016-11-22 23:18:18 -0200

    Unidade 4 - Aula 1 - Exemplo 2 - Início

--- MusicProject/MainViewController.swift
+++ MusicProject/MainViewController.swift
@@ -12,16 +12,6 @@ class MainViewController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        UITabBar.appearance().tintColor = #colorLiteral(red: 1, green: 0.2842617035, blue: 0.4058894515, alpha: 1)
-        ((tabBar.items?[0])! as UITabBarItem).image = #imageLiteral(resourceName: "ic_search").withRenderingMode(.alwaysOriginal)
-        ((tabBar.items?[1])! as UITabBarItem).image = #imageLiteral(resourceName: "ic_artist").withRenderingMode(.alwaysOriginal)
-        ((tabBar.items?[2])! as UITabBarItem).image = #imageLiteral(resourceName: "ic_music").withRenderingMode(.alwaysOriginal)
-        
-        ((tabBar.items?[0])! as UITabBarItem).selectedImage = #imageLiteral(resourceName: "ic_search_selected").withRenderingMode(.alwaysOriginal)
-        ((tabBar.items?[1])! as UITabBarItem).selectedImage = #imageLiteral(resourceName: "ic_artist_selected").withRenderingMode(.alwaysOriginal)
-        ((tabBar.items?[2])! as UITabBarItem).selectedImage = #imageLiteral(resourceName: "ic_music_selected").withRenderingMode(.alwaysOriginal)
-        
     }
     
 }
