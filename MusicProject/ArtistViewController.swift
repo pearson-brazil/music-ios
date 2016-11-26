5541a052c95c146d96385b62e22016f1eeda2789
Pearson <w.policiano@gmail.com>
2016-11-26 16:19:15 -0200

    Tela de artistas criada

--- MusicProject/ArtistViewController.swift
+++ MusicProject/ArtistViewController.swift
@@ -10,7 +10,36 @@ import UIKit
 
 class ArtistViewController: UIViewController {
 
-    
+    let items : [String] = ["Artista 1", "Artista 2", "Artista 3", "Artista 4", "Artista 5", "Artista 6", "Artista 7", "Artista 8", "Artista 9", "Artista 10", "Artista 11", "Artista 12", "Artista 13", "Artista 14"]
     
 
+}
+
+extension ArtistViewController: UITableViewDataSource, UITableViewDelegate {
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCellIdentifier", for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70.0
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "LISTA DE ARTISTAS"
+    }
 }
