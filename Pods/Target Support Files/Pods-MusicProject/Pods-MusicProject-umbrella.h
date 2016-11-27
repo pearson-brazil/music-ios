c1a8744d757c52e93dfc4a443ba4ae8038aa3521
Pearson <w.policiano@gmail.com>
2016-11-27 09:58:55 -0200

    Adiciona Firebase

---
+++ Pods/Target Support Files/Pods-MusicProject/Pods-MusicProject-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_MusicProjectVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MusicProjectVersionString[];
+
