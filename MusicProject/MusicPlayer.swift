68e44fc3b78291abcc5293778758a07f1a62829d
Pearson <w.policiano@gmail.com>
2017-01-04 23:56:35 -0200

    Aula 3 finalizada

---
+++ MusicProject/MusicPlayer.swift
@@ -0,0 +1,13 @@
+//
+//  MusicPlayer.swift
+//  MusicProject
+//
+//  Created by Pearson on 04/01/17.
+//  Copyright © 2017 Pearson. All rights reserved.
+//
+
+import UIKit
+
+class MusicPlayer: NSObject {
+
+}
